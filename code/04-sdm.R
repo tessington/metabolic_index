3c9fcbecd05b143a1f297f552e01bb865b023802
Tim Essington <tessington@users.noreply.github.com>
2025-11-11 17:33:26 -0800

    revise sdm using cross validation and two new species

--- code/04-sdm.R
+++ code/04-sdm.R
@@ -6,6 +6,11 @@ library(INLA)
 library(sdmTMB)
 library(ggeffects)
 library(purrr)
+library(TMB)
+library(worrms)
+library(blockCV)
+library(future)
+
 #Load functions
 source("code/helper/fit_model_funs.R")
 
@@ -63,16 +68,15 @@ dover_sole <- dplyr::filter(dat, common_name == "dover sole")
 pacific_hake <- dplyr::filter(dat, common_name == "pacific hake")
 pacific_cod <- dplyr::filter(dat, common_name == "pacific cod")
 pacific_halibut <- dplyr::filter(dat, common_name == "pacific halibut")
-
+longspine_thornyhead <- dplyr::filter(dat, common_name == "longspine thornyhead")
+canary_rockfish <- dplyr::filter(dat, common_name == "canary rockfish")
 # function to streamline code.  Calculate P(po2 > pcrit) for each row of dataframe
 update_df <- function(species_df, taxa.name, w, pcrit_type = "smr", rep, ParentChild_gz) {
 # get parameter estimates
   taxa_estimates <- estimate_taxa(taxa.name, 
                                      w = w ,
                                      temperature = 10,
-                                     method = pcrit_type,
-                                     rep  = model.fit$rep, 
-                                     ParentChild_gz = model.fit$ParentChild_gz)
+                                     method = pcrit_type)
   # use pmap to run calc_p_po2
   betas <- taxa_estimates$parameters[,1]
   var_covar <- taxa_estimates$var_covar
@@ -109,7 +113,14 @@ taxa.name <- "Microstomus pacificus"
 dover_sole <- update_df(dover_sole, taxa.name, pcrit_type = pcrit_type, w = W,model.fit$rep, model.fit$ParentChild_gz)
 dover_sole$cpue_weight <- with(dover_sole, catch_weight / effort)
 
-# filter data by depth of 97.5 percent of occurences
+# Longspine thornyhead
+taxa.name <- "Sebastolobus altivelis"
+longspine_thornyhead <- update_df(longspine_thornyhead, taxa.name, pcrit_type = pcrit_type, w = W,model.fit$rep, model.fit$ParentChild_gz)
+
+taxa.name <- "Sebastes pinniger"
+canary_rockfish <- update_df(canary_rockfish, taxa.name, pcrit_type = pcrit_type, w = W,model.fit$rep, model.fit$ParentChild_gz)
+
+# filter data by depth of threshold percent of occurrences
 # function
 trim_depths <- function(data.2.use, threshold) {
   number_occurences <- sum(data.2.use$present_absent)
@@ -128,124 +139,150 @@ pacific_hake <- trim_depths(data.2.use = pacific_hake,
                                threshold = depth_threshold)
 dover_sole <- trim_depths(data.2.use = dover_sole,
                                threshold = depth_threshold)
+longspine_thornyhead <- trim_depths(data.2.use = longspine_thornyhead,
+                          threshold = depth_threshold)
+canary_rockfish <- trim_depths(data.2.use = canary_rockfish,
+                          threshold = depth_threshold)
 
-# Function to fit species distribution model ####
-fit_model <- function(data.2.use, formula_2_use, includepo2 = T) {
-  data.2.use$log_depth_scaled <- scale(log(data.2.use$depth))
+# Functions to fit species distribution model via cross validation ####
+build_mesh <- function(data.2.use, cutoff = 10) {
+  bnd <- INLA::inla.nonconvex.hull(cbind(data.2.use$X, data.2.use$Y), convex = -0.05)
+  inla_mesh <- INLA::inla.mesh.2d(boundary = bnd, max.edge = c(150, 1000),
+                                  offset = -0.1, cutoff = NULL, min.angle = 5)
+  sdmTMB::make_mesh(data.2.use, c("X","Y"), mesh = inla_mesh)
+}
+
+fit_model <- function(data.2.use, formula_2_use, spde, modeltype = "base", max_threshold = NULL) {
+  print(modeltype)
+  # setup sdmTMB control specifications
+  if (modeltype == "base") {
+    control <- sdmTMBcontrol(multiphase = TRUE) 
+  }
   
-  #Make mesh
-  library(INLA)
-  library(sdmTMB)
-  bnd <- INLA::inla.nonconvex.hull(cbind(data.2.use$X, data.2.use$Y), 
-                                   convex = -0.05)
-  inla_mesh <- INLA::inla.mesh.2d(
-    boundary = bnd,
-    max.edge = c(150, 1000),
-    offset = -0.1, # default -0.1
-    cutoff = NULL,
-    min.angle = 5 # default 21
-  )
-  spde <- make_mesh(data.2.use, c("X", "Y"), mesh = inla_mesh)
-  if (includepo2) {
+  if (modeltype == "po2") {
     # Get the number of fixed effects parameters and set lower bound for the po2 effect (always second)
     nfixed_effects <- 4 + length(unique(data.2.use$survey)) -1 
     lower_bound_array <- rep(-Inf, nfixed_effects)
     upper_bound_array <- rep(Inf, nfixed_effects)
     lower_bound_array[2] <- 0 # Set lower bound for the ppcrit fixed effect to 0
+    control = sdmTMBcontrol(lower = list(b_j = lower_bound_array), 
+                            upper = list(b_j = upper_bound_array), 
+                            newton_loops = 0,
+                            multiphase = TRUE)
+  } 
+ 
+  if (modeltype == "threshold") {
+    lower_bound_array = rep(-Inf, 2)
+    upper_bound_array <- rep(Inf, 2)
+    lower_bound_array[1] <- 0
+    if(!is.null(max_threshold)) {
+      upper_bound_array[2] <- max_threshold
+    }
     
-    model <- sdmTMB(formula_2_use, 
-                    mesh = spde,
-                    family = binomial(),
-                    data = data.2.use,
-                    spatial = "on",
-                    anisotropy = TRUE,
-                    extra_time = c(2016, 2020),
-                    time = "year",
-                    spatiotemporal = "iid",
-                    control = sdmTMBcontrol(lower = list(b_j = lower_bound_array), 
-                                            upper = list(b_j = upper_bound_array), 
-                                            newton_loops = 0,
-                                            multiphase = TRUE)
-                    )
-  } else {
-    model <- sdmTMB(formula_2_use, 
-                    mesh = spde,
-                    family = binomial(),
-                    data = data.2.use,
-                    spatial = "on",
-                    anisotropy = TRUE,
-                    extra_time = c(2016, 2020),
-                    time = "year",
-                    spatiotemporal = "iid",
-                    control = sdmTMBcontrol(multiphase = TRUE)
-                    )
-    
+    control = sdmTMBcontrol(lower = list(b_threshold = lower_bound_array), 
+                            upper = list(b_threshold = upper_bound_array), 
+                            newton_loops = 0,
+                            multiphase = TRUE)
   }
+  
+#  future::plan(multisession)
+  cv_out <- sdmTMB_cv(
+    formula = formula_2_use,
+    data = data.2.use,
+    family = binomial(),
+    spatial = "on",
+    anisotropy = FALSE,
+    time = "year",
+    spatiotemporal = "iid",
+    extra_time = c(2016, 2020),
+    control = control,
+    fold_ids = data.2.use$fold,
+    mesh = spde
+  )
 
-  return(model)
+  return(cv_out)
 }
 
+make_spatial_blocks <- function(data.2.use, 
+                                block_size = 100,
+                                number_of_folds = 4) {
+  data.2.use.adjusted <- sf::st_as_sf(data.2.use, coords = c("X", "Y"), crs = NA)
+  spatial_blocking <- cv_spatial(
+    x = data.2.use.adjusted,
+    column = "present_absent",
+    size = block_size ,
+    k = number_of_folds,
+    selection = "random",
+    iteration = 50)
+  return_data <- data.2.use
+  return_data$folds <- spatial_blocking$folds_ids
+  return(return_data)
+}
+
+# Function to loop through models and save results
+
+fit_all_models <- function(data.2.use, seed) {
+  # transform predictors
+  data.2.use$log_depth_scaled <- scale(log(data.2.use$depth))
+  data.2.use$temp_scaled <- scale(data.2.use$temperature_C)
+  data.2.use$temp_scaled_squared <- with(data.2.use, scale(temperature_C^2) )
+  data.2.use$po2_scaled <- scale(data.2.use$po2)
+  data.2.use$p_pcrit_scaled <- scale(data.2.use$p_pcrit)
+  
+  # get spatial blocking
+  set.seed(seed)
+  data.2.use <- make_spatial_blocks(data.2.use, block_size = 65, number_of_folds = 5)
+  
+  # make mesh
+  spde <- build_mesh(data.2.use)
+  
+  base_model <- fit_model(data.2.use = data.2.use, 
+                          formula_2_use = formula(present_absent ~ s(log_depth_scaled) + temp_scaled + temp_scaled_squared + survey),
+                          modeltype = "base",
+                          spde = spde)
+  ppcrit_model <- fit_model(data.2.use = data.2.use,
+                            formula_2_use = formula(present_absent ~ breakpt(p_pcrit_scaled)  + s(log_depth_scaled)  +  temp_scaled + temp_scaled_squared  + survey),
+                            modeltype = "threshold",
+                            spde = spde,
+                            max_threshold = max(data.2.use$p_pcrit_scaled)
+                            )
+  
+  po2_model <- fit_model(data.2.use = data.2.use,
+                          formula_2_use = formula(present_absent ~ breakpt(po2_scaled)  + s(log_depth_scaled) + temp_scaled + temp_scaled_squared  + survey),
+                         modeltype ="threshold",
+                         spde = spde,
+                         max_threshold = max(data.2.use$po2_scaled))
+  
+  return(list(base = base_model, ppcrit = ppcrit_model, po2= po2_model, cv_scores = c(base_model$sum_loglik,
+                                                                 ppcrit_model$sum_loglik,
+                                                                 po2_model$sum_loglik) ) )
+  
+}
+  
+  
+
 # Fit models to each species ####
-## Pacific Cod ####
-
-formula_2_use <- formula(present_absent ~ s(log_depth_scaled) + scale(temperature_C) + survey)
-
-base_model_pacific_cod <- fit_model(data.2.use = pacific_cod, 
-                               formula_2_use = formula_2_use,
-                               includepo2 = FALSE)
-formula_2_use <- formula(present_absent ~ scale(p_pcrit)  + s(log_depth_scaled)  + scale(temperature_C)  + survey)
-
-ppcrit_model_pacific_cod <- fit_model(data.2.use = pacific_cod,
-                                      formula_2_use = formula_2_use)
-po2_model_pacific_cod <- fit_model(data.2.use = pacific_cod,
-                                      formula_2_use = formula(present_absent ~ scale(po2)  + s(log_depth_scaled) + scale(temperature_C)  + survey))
-
-aic_list_pacific_cod <- c(AIC(base_model_pacific_cod), AIC(ppcrit_model_pacific_cod), AIC(po2_model_pacific_cod))
+## Pacific cod ####
+pacific_cod_fits  <- fit_all_models(data.2.use = pacific_cod, seed = 1311)
 ## Pacific halibut ####
-
-base_model_pacific_halibut <- fit_model(data.2.use = pacific_halibut, 
-                                        formula_2_use = formula(present_absent ~ s(log_depth_scaled) + scale(temperature_C) + survey),
-                                        includepo2 = F)
-ppcrit_model_pacific_halibut <-fit_model(data.2.use = pacific_halibut, 
-                                         formula_2_use = formula(present_absent ~ scale(p_pcrit) + s(log_depth_scaled) + scale(temperature_C) + survey),
-                                         includepo2 = T)
-po2_model_pacific_halibut <- fit_model(data.2.use = pacific_halibut, 
-                                    formula_2_use = formula(present_absent ~ scale(po2)  + s(log_depth_scaled) + scale(temperature_C) + survey),
-                                    includepo2 = T)
-aic_list_pacific_halibut <- c(AIC(base_model_pacific_halibut), AIC(ppcrit_model_pacific_halibut), AIC(po2_model_pacific_halibut))
+pacific_halibut_fits <- fit_all_models(data.2.use = pacific_halibut, seed = 1311)
 ## Pacific hake ####
-base_model_pacific_hake <- fit_model(data.2.use = pacific_hake, 
-                                        formula_2_use = formula(present_absent ~ s(log_depth_scaled) + scale(temperature_C) + survey),
-                                        includepo2 = F)
-ppcrit_model_pacific_hake <-fit_model(data.2.use = pacific_hake, 
-                                         formula_2_use = formula(present_absent ~ scale(p_pcrit) + s(log_depth_scaled) + scale(temperature_C) + survey),
-                                         includepo2 = T)
-po2_model_pacific_hake <- fit_model(data.2.use = pacific_hake, 
-                                       formula_2_use = formula(present_absent ~ scale(po2)  + s(log_depth_scaled) + scale(temperature_C) + survey),
-                                       includepo2 = T)
-aic_list_pacific_hake <- c(AIC(base_model_pacific_hake), AIC(ppcrit_model_pacific_hake), AIC(po2_model_pacific_hake))
-
+pacific_hake_fits <- fit_all_models(data.2.use = pacific_hake, seed = 1311)
 ## Dover sole ####
-base_model_dover_sole <- fit_model(data.2.use = dover_sole, 
-                                   formula_2_use = formula(present_absent ~ s(log_depth_scaled) + scale(temperature_C) + survey),
-                                   includepo2 = F)
+dover_sole_fits <- fit_all_models(data.2.use = dover_sole, seed = 1311)
+## Longspine thornyhead ####
+longspine_thornyhead_fits <- fit_all_models(data.2.use = longspine_thornyhead, seed = 1311)
+## Canary rockfish ####
+canary_rockfish_fits <- fit_all_models(data.2.use = canary_rockfish, seed = 1311)
 
-ppcrit_model_dover_sole <- fit_model(data.2.use = dover_sole, 
-                                      formula_2_use = formula(present_absent ~ scale(p_pcrit) + s(log_depth_scaled) + scale(temperature_C) + survey),
-                                      includepo2 = T)
-po2_model_dover_sole <- fit_model(data.2.use = dover_sole, 
-                                  formula_2_use = formula(present_absent ~ scale(po2)  + s(log_depth_scaled) + scale(temperature_C) + survey),
-                                  includepo2 = T)
-aic_list_dover_sole <- c(AIC(base_model_dover_sole), AIC(ppcrit_model_dover_sole), AIC(po2_model_dover_sole))
-
-
-
-# make master deltaAIC table ####
-aic_table <- matrix(0,nrow = 3, ncol = 4)
-colnames( aic_table ) <- c("Pacific cod", "Pacific halibut", "Pacific hake", "Dover sole")
-rownames( aic_table ) <- c("base", "ppcrit", "po2")
-aic_table[,"Pacific cod"] <- aic_list_pacific_cod - min(aic_list_pacific_cod)
-aic_table[,"Pacific halibut"] <- aic_list_pacific_halibut - min(aic_list_pacific_halibut)
-aic_table[,"Pacific hake"] <- aic_list_pacific_hake - min(aic_list_pacific_hake)
-aic_table[,"Dover sole"] <- aic_list_dover_sole - min( aic_list_dover_sole )
-print( aic_table )
+# make master CV table ####
+cv_table <- matrix(0,nrow = 6, ncol = 3)
+rownames( cv_table ) <- c("Pacific cod", "Pacific halibut", "Pacific hake", "Dover sole", "Longspine thornyhead", "Canary rockfish")
+colnames( cv_table ) <- c("base", "ppcrit", "po2")
+cv_table["Pacific cod",] <- pacific_cod_fits$cv_scores 
+cv_table["Pacific halibut",] <-pacific_halibut_fits$cv_scores
+cv_table["Pacific hake",] <- pacific_hake_fits$cv_scores
+cv_table["Dover sole",] <- dover_sole_fits$cv_scores
+cv_table["Longspine thornyhead",] <- longspine_thornyhead_fits$cv_scores
+cv_table["Canary rockfish",] <- canary_rockfish_fits$cv_scores
+print( cv_table )
